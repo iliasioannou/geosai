453ec4de490cc8547a91ba68b7df292d3999dbd6
Francesco Bruni <bruni@planetek.it>
2017-06-22 10:53:37 +0200

    Fixed catalogue url + removed geocoder filtering + centered map

--- cmems-gui/app/scripts/services/config.js
+++ cmems-gui/app/scripts/services/config.js
@@ -5,11 +5,11 @@ angular
 	.constant('configuration', {
 		//common environment configuration
 		rheticusHeaderImage : "./images/RheticusLogo.png",
-		map : {"center":{"lon":20.4,"lat":39.1,"zoom":8,"bounds":[],"projection":"EPSG:4326"},"view":{"projection":"EPSG:3857","maxZoom":19,"minZoom":3},"query":{"zoom":7},"crs":"EPSG:3857"},
+		map : {"center":{"lon":20.77,"lat":39.78,"zoom":6,"bounds":[],"projection":"EPSG:4326"},"view":{"projection":"EPSG:3857","maxZoom":19,"minZoom":3},"query":{"zoom":7},"crs":"EPSG:3857"},
 		dataProviders : [{"name":" Sentinel","checked":true},{"name":" Cosmo","checked":true},{"name":" TerraSAR-X","checked":true}],
 		geocoder : {"url":"http://nominatim.openstreetmap.org/search?q=","params":"&limit=5&format=json&accept-language=en&polygon_geojson=1","paramsReverse":"&limit=1&format=json&zoom=18&accept-language=en","urlReverse":"http://nominatim.openstreetmap.org/reverse?"},
 		//custom environment configuration
-		marineService : {"catalog":"http://cmems-catalog.rheticus.eu/geonetwork/srv/eng/catalog.search#/home","help":"./pdf/userguide_Marine.pdf"},
+		marineService : {"catalog":"http://marine-cmems.rheticus.eu/geonetwork/srv/eng/catalog.search#/home","help":"./pdf/userguide_Marine.pdf"},
 		CHL : {"dailySlider":"CHL","tenDaysSlider":"CHL10","monthSlider":"CHL30","month90Slider":"CHL30P"},
 		SST : {"dailySlider":"SST","tenDaysSlider":"SST10","monthSlider":"SST30","month90Slider":"SST30P"},
 		WT : {"dailySlider":"WT","tenDaysSlider":"WT10","monthSlider":"WT30","month90Slider":"WT30P"},
