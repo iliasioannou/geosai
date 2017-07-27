842a50b2a5e768d97ef4d585cb0bdc1a8405f3c5
Francesco Bruni <bruni@planetek.it>
2017-07-27 16:40:27 +0200

    Fixed unaligned label in time slider for 10 days

--- cmems-gui/config/environments/local/server.js
+++ cmems-gui/config/environments/local/server.js
@@ -13,9 +13,9 @@ server.get('/',function(req, res) {
  * PROXY CONFIGURATION
  */
 var HASH_MAP_EXTERNAL_SERVICES = {
-	"RHETICUS_API" : "http://localhost:3000",
-  "GEOSERVER" : "http://geoserver:9090",
-  "GEONETWORK" : "http://geonetwork:8080",
+	"RHETICUS_API" : "http://marine-cmems.rheticus.eu",
+  "GEOSERVER" : "http://marine-cmems.rheticus.eu",
+  "GEONETWORK" : "http://marine-cmems.rheticus.eu",
   "ACTIVITI": "http://activiti:9085",
   "DOWNLOAD": "http://nginx"
 };
