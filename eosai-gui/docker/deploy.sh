7e8edb34400a4324fb7124805f72dbd0edbcba4c
Francesco Bruni <bruni@planetek.it>
2017-12-04 11:43:31 +0100

    Fixed docker build call

--- eosai-gui/docker/deploy.sh
+++ eosai-gui/docker/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 docker build -t planetek/node-bower-compass-grunt:4 -f Dockerfile.base .
 docker build -t planetek/eosai-gui:$1 --build-arg branch=$1 -f Dockerfile.base.code .
-docker build -t planetek/eosai-gui:$1 --build-arg --build-arg branch=$1 -f Dockerfile.final .
+docker build -t planetek/eosai-gui:$1 --build-arg branch=$1 -f Dockerfile.final .
 # docker-compose up -d gui
