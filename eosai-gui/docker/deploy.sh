04636bba1f4e42f7d29f8faf7597d2947b1667d6
Francesco <brunifrancesco02@gmail.com>
2017-12-06 15:01:05 +0100

    Fixed image name

--- eosai-gui/docker/deploy.sh
+++ eosai-gui/docker/deploy.sh
@@ -4,5 +4,5 @@ docker build -t dockerhub.planetek.it/node-bower-compass-grunt:4 -f Dockerfile.b
 docker push dockerhub.planetek.it/node-bower-compass-grunt:4
 docker build -t dockerhub.planetek.it/pkh111_eosai-gui:base -f Dockerfile.base.code .
 docker push dockerhub.planetek.it/pkh111_eosai-gui:base
-docker build --no-cache -t planetek/eosai-gui:$1 --build-arg branch=$1 -f Dockerfile.final .
+docker build --no-cache -t dockerhub.planetek.it/pkh111_eosai-gui:$1 --build-arg branch=$1 -f Dockerfile.final .
 docker push dockerhub.planetek.it/pkh111_eosai-gui:$1
