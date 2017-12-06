3b9ce96a3a9929366cdd627d459fa7be7d89a9cd
Francesco <brunifrancesco02@gmail.com>
2017-12-06 10:05:00 +0100

    Fixed typo

--- eosai-gui/docker/deploy.sh
+++ eosai-gui/docker/deploy.sh
@@ -4,4 +4,4 @@ docker push dockerhub.planetek.it/node-bower-compass-grunt:4
 docker build -t dockerhub.planetek.it/pkh111_eosai-gui:base -f Dockerfile.base.code .
 docker push dockerhub.planetek.it/pkh111_eosai-gui:base
 docker build --no-cache -t planetek/eosai-gui:$1 --build-arg branch=$1 -f Dockerfile.final .
-docker psuh dockerhub.planetek.it/pkh111_eosai-gui:$1
+docker push dockerhub.planetek.it/pkh111_eosai-gui:$1
