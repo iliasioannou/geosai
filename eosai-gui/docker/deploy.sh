674ac0826d72b2d77e8a5fa091d8c28389c88c9e
Francesco <brunifrancesco02@gmail.com>
2017-12-06 11:52:25 +0100

    Exit from script in case of error

--- eosai-gui/docker/deploy.sh
+++ eosai-gui/docker/deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e 
 docker build -t dockerhub.planetek.it/node-bower-compass-grunt:4 -f Dockerfile.base .
 docker push dockerhub.planetek.it/node-bower-compass-grunt:4
 docker build -t dockerhub.planetek.it/pkh111_eosai-gui:base -f Dockerfile.base.code .
