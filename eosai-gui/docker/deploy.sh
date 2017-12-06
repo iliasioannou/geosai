df31fa73719fb117da42707e894b770050d37505
Francesco <brunifrancesco02@gmail.com>
2017-12-06 09:33:13 +0100

    Hubify image

--- eosai-gui/docker/deploy.sh
+++ eosai-gui/docker/deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-docker build -t planetek/node-bower-compass-grunt:4 -f Dockerfile.base .
-docker build -t planetek/eosai-gui:$1 --build-arg branch=$1 -f Dockerfile.base.code .
+docker build -t dockerhub.planetek.it/node-bower-compass-grunt:4 -f Dockerfile.base .
+docker push dockerhub.planetek.it/node-bower-compass-grunt:4
+docker build -t dockerhub.planetek.it/pkh111_eosai-gui:base -f Dockerfile.base.code .
+docker push dockerhub.planetek.it/pkh111_eosai-gui:base
 docker build --no-cache -t planetek/eosai-gui:$1 --build-arg branch=$1 -f Dockerfile.final .
-# docker-compose up -d gui
+docker psuh dockerhub.planetek.it/pkh111_eosai-gui:$1
